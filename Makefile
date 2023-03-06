72eff8a1001f2cf4674c720a43b73ba2327c418a
Michal Stachura <michal.stachura@santri.pl>
2023-03-06 12:25:04 +0100

    Add periodic task to delete overdue urls

--- Makefile
+++ Makefile
@@ -21,5 +21,8 @@ superuser:
 shell:
 	docker-compose -p sotiny -f local.yml run --rm django python manage.py shell
 
+bash:
+	docker-compose -p sotiny -f local.yml exec celeryworker bash
+
 cleardocker:
 	docker-compose -p sotiny -f local.yml down --volumes --remove-orphans --rmi all
--- sotinyurl/tinyurls/admin.py
+++ sotinyurl/tinyurls/admin.py
@@ -4,6 +4,11 @@ from sotinyurl.tinyurls.models import TinyUrl
 
 @admin.register(TinyUrl)
 class TinyUrlAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "valid_to", "owner"]
+    list_display = ["__str__", "valid_to", "owner", "deleted_at"]
     search_fields = ["id", "tiny_url", "redirect_to", "owner__username"]
     readonly_fields = ["tiny_url"]
+
+    def get_queryset(self, request):
+        if request.user.is_superuser:
+            return TinyUrl.objects.with_deleted()
+        return super().get_queryset(request)
